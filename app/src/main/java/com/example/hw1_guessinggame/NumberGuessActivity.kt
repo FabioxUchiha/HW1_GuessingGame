100200495965e41d38d4277c057d960c57d1123b
FabioxUchiha <36249220+FabioxUchiha@users.noreply.github.com>
2019-08-23 00:04:32 -0500

    primer commit taller 1 electiva

---
+++ app/src/main/java/com/example/hw1_guessinggame/NumberGuessActivity.kt
@@ -0,0 +1,75 @@
+package com.example.hw1_guessinggame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_number_guess.*
+import java.util.*
+import kotlin.random.Random as Random1
+
+class NumberGuessActivity : AppCompatActivity() {
+
+    private var ayuda = 0
+    private var jugadas = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_number_guess)
+        iniciarJuego()
+    }
+
+    private fun iniciarJuego() {
+
+        val r = Random()
+        ayuda = r.nextInt(1000) + 1
+        Toast.makeText(this, "el random es: "+ayuda, Toast.LENGTH_SHORT).show()
+        jugadas = 0
+        btnIniciar.isEnabled = true
+        ingresaNumero.text.clear()
+        txtMensaje.text = ""
+    }
+
+    fun jugarClick(view: View) {
+
+        val jugar: Int
+
+        try {
+            jugar = ingresaNumero.text.toString().toInt()
+        } catch (e: Exception) {
+            valorJuego()
+            return
+        }
+
+        if (jugar !in 1..1000) {
+            valorJuego()
+            return
+        }
+
+        jugadas++
+
+        when {
+            jugar < ayuda -> txtMensaje.text = getString(R.string.alto)
+            jugar > ayuda -> txtMensaje.text = getString(R.string.bajo)
+            else -> {
+                txtMensaje.text = "el numero de intentos usados fue: $jugadas"
+                ingresaNumero.isEnabled = false
+            }
+        }
+    }
+
+    private fun valorJuego() {
+        Toast.makeText(this, "DEBE INGRESAR UN NUMERO ENTRE UNO Y MIL!", Toast.LENGTH_SHORT).show()
+        ingresaNumero.text.clear()
+        txtMensaje.text = ""
+    }
+
+    fun iniciarClick(view: View) {
+
+        iniciarJuego()
+    }
+
+}
