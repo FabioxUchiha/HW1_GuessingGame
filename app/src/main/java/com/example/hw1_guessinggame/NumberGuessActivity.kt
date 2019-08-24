ad73014780ded013e6c020a01568659e3badd84d
FabioxUchiha <36249220+FabioxUchiha@users.noreply.github.com>
2019-08-23 22:36:00 -0500

    version final del juego, ajustes en los intentos y reinicio del juego

--- app/src/main/java/com/example/hw1_guessinggame/NumberGuessActivity.kt
+++ app/src/main/java/com/example/hw1_guessinggame/NumberGuessActivity.kt
@@ -15,6 +15,7 @@ class NumberGuessActivity : AppCompatActivity() {
 
     private var ayuda = 0
     private var jugadas = 0
+    private var intentos = 999
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,41 +23,63 @@ class NumberGuessActivity : AppCompatActivity() {
         iniciarJuego()
     }
 
+
     private fun iniciarJuego() {
-        val r = Random()
-        ayuda = r.nextInt(1000) + 1
-        //Toast.makeText(this, "el random es: "+ayuda, Toast.LENGTH_SHORT).show()
-        jugadas = 0
-        btnIniciar.isEnabled = true
-        ingresaNumero.text.clear()
-        txtMensaje.text = ""
+
+            val r = Random()
+            ayuda = r.nextInt(1000) + 1
+            //Toast.makeText(this, "el random es: "+ayuda, Toast.LENGTH_SHORT).show()
+            jugadas = 0
+
+            ingresaIntentos.text.clear()
+            btnIniciar.isEnabled = true
+            ingresaNumero.text.clear()
+            txtMensaje.text = ""
+
     }
 
+
     fun jugarClick(view: View) {
-
-        val jugar: Int
-
-        try {
-            jugar = ingresaNumero.text.toString().toInt()
-        } catch (e: Exception) {
-            valorJuego()
-            return
-        }
-
-        if (jugar !in 1..1000) {
-            valorJuego()
-            return
-        }
-
-        jugadas++
-
-        when {
-            jugar < ayuda -> txtMensaje.text = getString(R.string.alto)
-            jugar > ayuda -> txtMensaje.text = getString(R.string.bajo)
-            else -> {
-                txtMensaje.text = "el numero de intentos usados fue: $jugadas"
-                ingresaNumero.isEnabled = false
+        if (intentos==999){
+            try {
+                intentos = ingresaIntentos.text.toString().toInt()
+            }catch (e: Exception){
+                valorIntentos()
+                return
             }
+        }
+
+
+        if (intentos<=0){
+            Toast.makeText(this, "se terminaron los intentos, presione iniciar para jugar denuevo: ", Toast.LENGTH_SHORT).show()
+        }else{
+            val jugar: Int
+            try {
+                jugar = ingresaNumero.text.toString().toInt()
+            } catch (e: Exception) {
+                valorJuego()
+                return
+            }
+
+            if (jugar !in 1..1000) {
+                valorJuego()
+                return
+            }
+
+            jugadas++
+            intentos=intentos-1
+            Toast.makeText(this, "su numero de intentos es: "+intentos, Toast.LENGTH_SHORT).show()
+
+
+
+            when {
+                jugar < ayuda -> txtMensaje.text = getString(R.string.alto)
+                jugar > ayuda -> txtMensaje.text = getString(R.string.bajo)
+                else -> {
+                    txtMensaje.text = "el numero de intentos usados fue: $jugadas"
+                    ingresaNumero.isEnabled = false
+                }
+        }
         }
     }
 
@@ -66,8 +89,14 @@ class NumberGuessActivity : AppCompatActivity() {
         txtMensaje.text = ""
     }
 
-    fun iniciarClick(view: View) {
+    private fun valorIntentos() {
+        Toast.makeText(this, "DEBE INGRESAR UN NUMERO DE INTENTOS!", Toast.LENGTH_SHORT).show()
+        ingresaNumero.text.clear()
+        txtMensaje.text = ""
+    }
 
+    fun iniciarClick(view: View) {
+        intentos=999
         iniciarJuego()
     }
 
